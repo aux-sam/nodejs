41ef74e3601472bd9a5552ccecb9057f53e7db95
Auxilium Technology <samer@auxiliumps.com>
2023-02-27 14:12:58 +0200

    added express.static

--- 02-ejs/server.js
+++ 02-ejs/server.js
@@ -5,6 +5,7 @@ const app = express();
 // EJS
 app.set("view engine","ejs")
 app.set('views',path.join(__dirname,'/pages'))
+app.use(express.static(path.join(__dirname,'/public'))); // set path for assets folder
 
 //Routes
 app.get("/", (req, res) => {
